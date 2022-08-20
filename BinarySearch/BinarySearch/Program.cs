3259cf80a8209aa484d184f09a3c2c78329f3572
Erfan Faridi <109587086+Erfan-ffa@users.noreply.github.com>
2022-08-20 20:28:30 +0430

    Add files via upload

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,20 @@
+﻿using BinarySearch;
+
+Console.Write("Key : ");
+int key = Convert.ToInt32(Console.ReadLine());
+
+do 
+{
+    RecursiveBinarySearch x = new RecursiveBinarySearch();
+    x.BinarySearch(key, 0, 9);
+
+    Console.WriteLine("-----------------------------");
+
+    IterativeBinarySearch a = new IterativeBinarySearch();
+    a.BinarySearchs(key);
+
+
+    Console.Write("Key : ");
+    key = Convert.ToInt32(Console.ReadLine());
+
+} while (key != -50);
